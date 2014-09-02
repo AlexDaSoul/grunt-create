f36fdc3a126d989b2c7e264f1cbb0c2f33c3f597
al-teen@yandex.ru <al-teen@yandex.ru>
2014-09-02 13:39:00 +0400

    init git

---
+++ README.md
@@ -0,0 +1,48 @@
+# grunt-create
+
+> Create files and folders with name from flag
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-create --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-create');
+```
+
+## The "create" task
+
+### Overview
+In your project's Gruntfile, add a section named `create` to the data object passed into `grunt.initConfig()`.
+
+	var spec = 'describe("Simple object", function() {\n    it("should say hi", function() {\n\r    });\n});'
+	var script = '(function() {\n\r}()};'
+	
+ 		create: {
+			options: {
+				files: [
+					{
+						'type': 'js',
+						'content': script
+					},
+					{
+						'type': 'spec.js',
+						'content': spec
+					}
+				]
+			},
+			puth: {
+				src: ['js/', 'spec/']
+			}
+		},
+		
+	grunt.loadNpmTasks('grunt-create');
+	
+	grunt create --new=newproject
---
+++ tasks/create.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+
+	var util = grunt.util || grunt.utils,
+		_ = util._;
+
+	grunt.registerMultiTask('create', function(prop) {
+
+		var taskDone = this.async(),
+			name = grunt.option('new'),
+			targetFiles = (_.isUndefined(this.data.src)) ? this.data : this.data.src,
+			options = this.options({
+				files: []
+			});
+
+		
+		for (i = 0; i < targetFiles.length; i++)
+		{
+			var filePath = targetFiles[i],
+				optdir = grunt.template.process(filePath);
+				
+			grunt.log.write('\nCreated ' + filePath + name + '/\n-------------\n\n');
+			
+
+			if (!grunt.file.exists(filePath))
+			{	
+				grunt.log.error('Folder ' + filePath + ' not found!!!');
+				
+				return;
+			}
+			
+			if (grunt.file.exists(optdir + '/' + name))
+			{	
+				grunt.log.error('Project ' + name + ' already exist!');
+				
+				return;
+			}			
+					
+			grunt.file.mkdir(optdir + '/' + name, '0777');
+					
+			grunt.log.ok(filePath + name + ' created successfully');
+			
+ 			for (f = 0; f < options.files.length; f++)
+			{
+				var fileType = options.files[f].type,
+					fileContent = options.files[f].content;
+
+				fileType = grunt.template.process(fileType);			
+				
+				if (f === i)
+				{
+					grunt.file.write(filePath + '/' + name + '/' + name + "." + fileType, fileContent, '0777');
+					grunt.log.ok(filePath + name + '.' + fileType + ' created successfully');
+				}
+
+			}; 
+			taskDone();
+		};
+	});
+};
